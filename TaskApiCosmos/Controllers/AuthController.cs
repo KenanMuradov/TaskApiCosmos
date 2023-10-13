d25da901a564d01b3d3d200e04d59f70f4cb4273
Kenan Muradov <maljerov.1999@gmail.com>
2023-10-13 21:01:33 +0400

    Add project files.

---
+++ TaskApiCosmos/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskApiCosmos.Models.DTO;
+using TaskApiCosmos.Services.Interfaces;
+
+namespace TaskApiCosmos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IJWTService _jwtService;
+
+        public AuthController(IUserService userService, IJWTService jwtService)
+        {
+            _userService = userService;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<string>> Register([FromForm] RegisterDTO request)
+        {
+            var existingUser = await _userService.FindUserByEmailAsync(request.Email);
+            if (existingUser is not null)
+                return Conflict("User already exists");
+
+
+            var user = await _userService.RegisterAsync(request);
+            if (user is not null)
+                return GenerateAccesToken(user.Id.ToString(), user.Email);
+
+            return BadRequest(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginDTO request)
+        {
+            var user = await _userService.FindUserByEmailAsync(request.Email);
+            if (user is null)
+                return Conflict("User doesn't exists");
+
+            var result = await _userService.LoginAsync(request);
+
+            if (!result)
+                return Conflict("Password or email is incorrect");
+
+            return GenerateAccesToken(user.Id.ToString(), user.Email);
+        }
+
+        private string GenerateAccesToken(string id, string email)
+        {
+            var accessToken = _jwtService.GenerateSecurityToken(id, email);
+            return accessToken;
+        }
+    }
+}
---
+++ TaskApiCosmos/Controllers/ProfileController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskApiCosmos.Data;
+using TaskApiCosmos.Services.Interfaces;
+
+namespace TaskApiCosmos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IStorageManager _storageManager;
+        private readonly AppDbContext _context;
+
+        public ProfileController(IUserService userService, IStorageManager storageManager, AppDbContext context)
+        {
+            _userService = userService;
+            _storageManager = storageManager;
+            _context = context;
+        }
+
+        [HttpGet("profilePhoto")]
+        public async Task<ActionResult<bool>> GetPPAsync(string email)
+        {
+            var user = await _userService.FindUserByEmailAsync(email);
+            if (user is null)
+                return false;
+
+            var url = _storageManager.GetSignedUrl(user.ProfilePhoto);
+            return Ok(url);
+        }
+        [AllowAnonymous]
+        [HttpGet("get")]
+        public IActionResult Get()
+        {
+            var users = _context.Users?.ToList();
+            return Ok(users);
+        }
+    }
+}
